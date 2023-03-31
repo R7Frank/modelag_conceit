49244eef1cbfbf663510d6571ecb83124a3635f4
R7frank <r7frank@hotmail.com>
2023-03-31 00:28:38 -0300

    Projeto criado

---
+++ src/main/java/com/r7frank/modelag_conceit/ModelagConceitApplication.java
@@ -0,0 +1,13 @@
+package com.r7frank.modelag_conceit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelagConceitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelagConceitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/r7frank/modelag_conceit/ModelagConceitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.r7frank.modelag_conceit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelagConceitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
