9972c7dbd2cd2417761713520102066e5fadfdbf
R7frank <r7frank@hotmail.com>
2023-03-31 01:24:11 -0300

    Classe de dominio Categoria: criada e testada

--- src/main/java/com/r7frank/modelag_conceit/resources/CategoriaResource.java
+++ src/main/java/com/r7frank/modelag_conceit/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.r7frank.modelag_conceit.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.r7frank.modelag_conceit.domain.Categoria;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String listar() {
-		return "Rest está no ar!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+				
+		return lista;
 		
 	}
 	
