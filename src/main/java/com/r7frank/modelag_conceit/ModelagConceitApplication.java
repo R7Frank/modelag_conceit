147c7c0885752cae0e5d78908560ff4835869f8a
R7frank <r7frank@hotmail.com>
2023-04-19 15:49:55 -0300

    Criadas: Cliente, TipoCliente, Telefones e Enderecos

--- src/main/java/com/r7frank/modelag_conceit/ModelagConceitApplication.java
+++ src/main/java/com/r7frank/modelag_conceit/ModelagConceitApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.r7frank.modelag_conceit.domain.Categoria;
 import com.r7frank.modelag_conceit.domain.Cidade;
+import com.r7frank.modelag_conceit.domain.Cliente;
+import com.r7frank.modelag_conceit.domain.Endereco;
 import com.r7frank.modelag_conceit.domain.Estado;
 import com.r7frank.modelag_conceit.domain.Produto;
+import com.r7frank.modelag_conceit.domain.enums.TipoCliente;
 import com.r7frank.modelag_conceit.repositories.CategoriaRepository;
 import com.r7frank.modelag_conceit.repositories.CidadeRepository;
+import com.r7frank.modelag_conceit.repositories.ClienteRepository;
+import com.r7frank.modelag_conceit.repositories.EnderecoRepository;
 import com.r7frank.modelag_conceit.repositories.EstadoRepository;
 import com.r7frank.modelag_conceit.repositories.ProdutoRepository;
 
@@ -24,9 +29,14 @@ public class ModelagConceitApplication implements CommandLineRunner {
 	@Autowired
 	private ProdutoRepository prodRep;
 	@Autowired
-	private EstadoRepository estado;
+	private EstadoRepository estadoRep;
 	@Autowired
-	private CidadeRepository cidade;
+	private CidadeRepository cidadeRep;
+	@Autowired
+	private ClienteRepository clienteRep;
+	@Autowired
+	private EnderecoRepository enderecoRep;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagConceitApplication.class, args);
@@ -62,8 +72,19 @@ public class ModelagConceitApplication implements CommandLineRunner {
 		catRep.saveAll(Arrays.asList(cat1, cat2));
 		prodRep.saveAll(Arrays.asList(p1, p2, p3));	
 		
-		estado.saveAll(Arrays.asList(est1, est2));
-		cidade.saveAll(Arrays.asList(c1, c2, c3));
+		estadoRep.saveAll(Arrays.asList(est1, est2));
+		cidadeRep.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRep.saveAll(Arrays.asList(cli1));
+		enderecoRep.saveAll(Arrays.asList(e1, e2));
 		
 	}
 
---
+++ src/main/java/com/r7frank/modelag_conceit/repositories/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.r7frank.modelag_conceit.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.r7frank.modelag_conceit.domain.Endereco;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
