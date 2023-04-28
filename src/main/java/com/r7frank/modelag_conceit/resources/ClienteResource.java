ae5fbd239f2a7a95d6a3794e0a20f5439e3b980a
R7frank <r7frank@hotmail.com>
2023-04-28 08:35:05 -0300

    Criados os metodos PUT, DELETE e GET para Cliente

--- src/main/java/com/r7frank/modelag_conceit/resources/ClienteResource.java
+++ src/main/java/com/r7frank/modelag_conceit/resources/ClienteResource.java
@@ -1,16 +1,26 @@
 package com.r7frank.modelag_conceit.resources;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.r7frank.modelag_conceit.domain.Cliente;
+import com.r7frank.modelag_conceit.dto.ClienteDTO;
 import com.r7frank.modelag_conceit.services.ClienteService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/clientes")
@@ -27,4 +37,39 @@ public class ClienteResource {
 		
 	}
 
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
+		Cliente obj = clienteService.fromDTO(objDto);
+		obj.setId(id);
+		obj = clienteService.update(obj);
+		return ResponseEntity.noContent().build();
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+	clienteService.delete(id);
+	return ResponseEntity.noContent().build();
+		
+	}
+	
+	@GetMapping()
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<Cliente> list = clienteService.findAll();
+		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());  
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@GetMapping("/page")
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage,
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy,
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		Page<Cliente> list = clienteService.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));  
+		return ResponseEntity.ok().body(listDto);
+	}	
+	
+	
 }
--- src/main/java/com/r7frank/modelag_conceit/services/ClienteService.java
+++ src/main/java/com/r7frank/modelag_conceit/services/ClienteService.java
@@ -1,11 +1,17 @@
 package com.r7frank.modelag_conceit.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.r7frank.modelag_conceit.domain.Cliente;
+import com.r7frank.modelag_conceit.dto.ClienteDTO;
 import com.r7frank.modelag_conceit.repositories.ClienteRepository;
 import com.r7frank.modelag_conceit.services.exceptions.ObjectNotFoundException;
 
@@ -21,4 +27,41 @@ public class ClienteService {
 		+ id + ", Tipo: " + Cliente.class.getName()));
 	}
 
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return cliRepos.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			cliRepos.deleteById(id);
+	}
+		catch(DataIntegrityViolationException e) {
+			throw new com.r7frank.modelag_conceit.services.exceptions.DataIntegrityViolationException("Não é permitido excluir um cliente que contenha entidades relacionadas a ele!");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return cliRepos.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return cliRepos.findAll(pageRequest);
+		
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+		
+	}	
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+	
+	
 }
